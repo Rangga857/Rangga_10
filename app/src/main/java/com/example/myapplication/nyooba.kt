48f582e59a3eb02261551aefa45ca56d0924c7d0
Rangga857 <azharrangga28@gmail.com>
2024-10-12 20:31:56 +0700

    coba

---
+++ app/src/main/java/com/example/myapplication/nyooba.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+fun main() {
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: &intValue")
+
+
+}
